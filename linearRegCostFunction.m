7417d9faccc69eb06e7a2df5df45d9ab9bbda3c5
Eugene Lin <eugenelin89@Eugenes-MacBook-Pro.local>
2013-12-07 02:00:53 -0800

    regularized cost and grad

--- linearRegCostFunction.m
+++ linearRegCostFunction.m
@@ -19,6 +19,23 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+H = X * theta;
+sqrErrors = (H - y).^2;
+term1 = (1/(2*m))*sum(sqrErrors);
+theta_mod = theta([2:size(theta,1)],:); %skip theta_0 (or in octave, theta_1)
+term2 = (lambda/(2*m))*sum( theta_mod.^2  );
+J = term1 + term2;
+
+% unregularized grad.  We will later just take the fist row (for theta-zero)
+grad = (1/m) * ((H-y)'*X)';
+regTerm = (lambda/m) * theta;
+% the first theta term will remail the same.  Save it first.
+grad1 = grad(1);
+% add the reg term to grad.  We will discard and replace the first row.
+grad = grad + regTerm;
+% then we replace the first row (for theta-zero)
+grad(1) = grad1;
+
 
 
 
